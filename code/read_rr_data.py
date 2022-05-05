cf084a96ba583907cc8870cc5df74b438f2667df
Lena Mueller <lenamariemue@gmail.com>
2022-05-05 14:06:50 +0200

    Split precipitation time series at dry measurement

--- code/read_rr_data.py
+++ code/read_rr_data.py
@@ -27,10 +27,10 @@ if len(datafiles) < nb_stations:
     sys.exit("Missing data files.")
 
 def get_p_for_zone(zone):
-    p_zone = []
-    zone_missing_rows = 0
+    p_zone = {"10":[], "20":[], "30":[], "40":[], "50":[], "60":[], "70":[], "80":[], 
+              "90":[], "100":[], "110":[], "120":[], "130":[], "140":[], "inf":[]}
     for station in zone:
-        station_missing_rows = 0
+        
         # read precipitation data file
         data_temp = pd.read_csv(f"data/DWD_RR_Stationen/p_{str(station).zfill(5)}.txt", sep=';',header=0)
         
@@ -38,29 +38,42 @@ def get_p_for_zone(zone):
         data_temp["MESS_DATUM"] = [datetime.datetime.strptime(str(x), '%Y%m%d%H%M') for x in data_temp["MESS_DATUM"].tolist()]
         
         for date in dates_nwccw:
-            # select rows matching date
+            # select rows matching the date
             date_plus1 = date + datetime.timedelta(days=1)
-            p_data = data_temp.loc[data_temp["MESS_DATUM"] < date_plus1]
-            p_data = p_data.loc[p_data["MESS_DATUM"] >= date]
-            p_data = p_data["RWS_10"].tolist()
-            # check for missing rows, fill them with 0mm and add values to p_zone
-            if len(p_data) == 144:
-                p_zone.extend(p_data)
-            else:
-                missing = 144 - len(p_data)
-                zone_missing_rows += missing
-                station_missing_rows += missing
-                p_zone.extend(p_data + [0]*missing)
-        print("station:", station, " - missing rows:", station_missing_rows)
-    print("zone missing rows:", zone_missing_rows)
-    # save p_zone into file
+            p_data_untilday = data_temp.loc[data_temp["MESS_DATUM"] < date_plus1]
+            p_data_subset = p_data_untilday.loc[p_data_untilday["MESS_DATUM"] >= date]
+            p_data_subset = np.array(p_data_subset["RWS_10"].tolist())
+        
+            
+            # add zero to end
+            list_with_zeros = p_data_subset
+            
+            # add zero to begin and end
+            list_with_zeros = [0] + list_with_zeros + [0]
+            
+            # get indices of zeros
+            indices = []
+            for i in range(len(list_with_zeros)):
+                if list_with_zeros[i] == 0:
+                    indices.append(i)
+
+            # slice list at zero indices
+            for i in range(len(indices)-1):
+                sublist = list_with_zeros[indices[i]+1:indices[i+1]]
+                
+                # check if list is not empty and allocate lists to dict
+                if 0 < len(sublist) < 15:
+                    duration = str(len(sublist)*10)
+                    p_zone[duration].append(sublist)
+                if len(sublist) >= 15:
+                    p_zone["inf"].append(sublist)
     return p_zone
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=24) as executor:
-    results = executor.map(get_p_for_zone, [stations_z1, stations_z2, stations_z3])
+dict_z1 = get_p_for_zone(stations_z1)
+dict_z2 = get_p_for_zone(stations_z2)
+dict_z3 = get_p_for_zone(stations_z3)
 
-results_arr = np.array(list(results))
      
-np.savetxt(f"data/DWD_RR_Stationen/p_z1", results_arr[0], delimiter=",", fmt='%1.2f')
-np.savetxt(f"data/DWD_RR_Stationen/p_z2", results_arr[1], delimiter=",", fmt='%1.2f')
-np.savetxt(f"data/DWD_RR_Stationen/p_z3", results_arr[2], delimiter=",", fmt='%1.2f')
+np.savetxt(f"data/DWD_RR_Stationen/p_z1", dict_z1, delimiter=",", fmt='%1.2f')
+np.savetxt(f"data/DWD_RR_Stationen/p_z2", dict_z2, delimiter=",", fmt='%1.2f')
+np.savetxt(f"data/DWD_RR_Stationen/p_z3", dict_z3, delimiter=",", fmt='%1.2f')
