2804651973644be1e2d5fff33a277e6cd525d369
Lena Mueller <lenamariemue@gmail.com>
2022-05-18 18:45:16 +0200

    Add unzipping routine to CDC_download.sh

--- code/CDC_download.sh
+++ code/CDC_download.sh
@@ -1,2 +1,4 @@
-# Download data from DWD`s open data server.
-wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical/ -P /home/lena/Documents/Master/Sem_2/FachvorträgeHydro/orographic_precipitation/data
+# Download and unzip data from DWD`s open data server.
+wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical/ -P /home/lena/Documents/Master/Sem_2/FachvorträgeHydro/orographic_precipitation/metdata
+cd /home/lena/Documents/Master/Sem_2/FachvorträgeHydro/orographic_precipitation/metdata/opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical
+unzip \*.zip
