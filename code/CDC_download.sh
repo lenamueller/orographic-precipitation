4195c09980d364e0ab12481292b6afa2269e3e26
Lena Mueller <lenamariemue@gmail.com>
2022-05-20 00:25:56 +0200

    Remove cd and unzip from CDC_download

--- code/CDC_download.sh
+++ code/CDC_download.sh
@@ -1,4 +1,2 @@
 # Download and unzip data from DWD`s open data server.
 wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical/ -P /home/lena/Documents/Master/Sem_2/FachvorträgeHydro/orographic_precipitation/metdata
-cd /home/lena/Documents/Master/Sem_2/FachvorträgeHydro/orographic_precipitation/metdata/opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical
-unzip \*.zip
