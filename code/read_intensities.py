be262689a04a1d27f7927726790b9b47dcc8abb2
Lena Mueller <lenamariemue@gmail.com>
2022-05-20 17:48:30 +0200

    Generalize path in read_intensities.py

--- code/read_intensities.py
+++ code/read_intensities.py
@@ -11,12 +11,12 @@ def read_intensities():
         z3 = pickle.load(f)
     return z1, z2, z3
 
-def read_intensities_st():
+def read_intensities_st(region):
     # output: dictionaries z1 = {"7394:[[],[],[],[],[],[],[]]"} with intensities in each list (window size)
-    with open('metdata/intensities/intensities_z1_st.pkl', 'rb') as f:
+    with open(f'metdata/intensities/intensities_{region}_z1.pkl', 'rb') as f:
         z1 = pickle.load(f)
-    with open('metdata/intensities/intensities_z2_st.pkl', 'rb') as f:
+    with open(f'metdata/intensities/intensities_{region}_z2.pkl', 'rb') as f:
         z2 = pickle.load(f)
-    with open('metdata/intensities/intensities_z3_st.pkl', 'rb') as f:
+    with open(f'metdata/intensities/intensities_{region}_z3.pkl', 'rb') as f:
         z3 = pickle.load(f)
     return z1, z2, z3
