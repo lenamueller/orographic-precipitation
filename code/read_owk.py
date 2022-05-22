896b4bea5099df1782c08333d0798338e287d766
Lena Mueller <lenamariemue@gmail.com>
2022-05-22 11:13:56 +0200

    Add owk_stats() and synoptic situations XX..F

--- code/read_owk.py
+++ code/read_owk.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import collections
 
 
 def read_owk():
@@ -12,6 +14,7 @@ def read_owk():
     # create subset which matches synoptic situations
     owk = {"NWAZF":[], "NWZAF":[], "NWZZF":[], # Erzgebirge
            "SWAZF":[], "SWZAF":[], "SWZZF":[], # Thüringer Wald, Harz
+           "XXZZF":[], "XXZAF":[], "XXAZF":[] # alle
            }
     for index, row in owk_df.iterrows():
         owk_i = row["owk_abbreviation"]
@@ -20,3 +23,16 @@ def read_owk():
             owk[owk_i].append(owk_datetime.date())
 
     return owk
+
+def owk_stats():
+    path_owk = '/home/lena/Documents/Master/Sem_2/FachvorträgeHydro/orographic_precipitation/metdata/owk/wlkvorhersage.txt'
+    owk_df = pd.read_csv(path_owk, sep=',', names=["datetime", "owk_number", "owk_abbreviation"], header=None)
+    data = owk_df["owk_abbreviation"]
+    freq = collections.Counter(data)
+    freq_sorted = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
+    names = list(freq_sorted.keys())
+    values = list(freq_sorted.values())
+    plt.bar(range(len(freq_sorted)), values, tick_label=names)
+    plt.title("Objektive Wetterklassifikation 12.09.1979 - 16.05.2022")
+    plt.xticks(rotation=90)
+    plt.savefig("images/owk_hist.png", dpi=300, bbox_inches="tight")
