0d48e0f1e224e2e16f0785febfc0a02379f23faa
Lena Mueller <lenamariemue@gmail.com>
2022-05-23 22:46:20 +0200

    Implement Marras strategy in rolling_intensity

--- code/rolling_window.py
+++ code/rolling_window.py
@@ -1,15 +1,15 @@
 def rolling_intensity(event:list[float], ws_list:list[int] = [1,2,3,6,12,18,36])->dict:
     max_pr_sum: dict = {ws: [] for ws in ws_list}
     for ws in ws_list:
-        # ! list must be at least as long as the window size
+        # list must be at least as long as the window size
         if ws <= len(event): 
             maxval: float = 0.0
             number_moves = len(event) - ws+1
             for i in range(number_moves):
                 window = event[i:i+ws]
-                val = max(window) # ! changed to max
+                val = sum(window)
                 if val > maxval:
                     maxval = val
-                    # ! use max window, scale to mm/10min and multiply with 6 to get mm/h
-                    max_pr_sum[ws].append((maxval*6))
+            # multiply with 6 to get mm/h
+            max_pr_sum[ws].append(maxval*6/ws)
     return max_pr_sum
